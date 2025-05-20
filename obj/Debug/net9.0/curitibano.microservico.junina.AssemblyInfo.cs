67dda34e5c49dd2a5d2580feca0686348790cfe4
Douglas Kerkhoven <douglas.kerkhoven@clubecuritibano.com.br>
2025-05-20 16:56:51 -0300

    Ajute para pegar o banco de dados

--- obj/Debug/net9.0/curitibano.microservico.junina.AssemblyInfo.cs
+++ obj/Debug/net9.0/curitibano.microservico.junina.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("curitibano.microservico.junina")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6d76729166cad400f1c4c557334940cf60a56a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b14f30014ce5894be86337517b371005d3c4f94")]
 [assembly: System.Reflection.AssemblyProductAttribute("curitibano.microservico.junina")]
 [assembly: System.Reflection.AssemblyTitleAttribute("curitibano.microservico.junina")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
